91c8df3160302feb66fdd56cedc7aff362bc36ff
Greg Doud <gpdoud@gmail.com>
2021-01-14 17:46:07 -0500

    Add project files.

---
+++ AocPingKeepAlive/Formation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AocPingKeepAlive {
+    class Formation {
+
+        [JsonInclude]
+        public int Id { get; set; }
+        [JsonInclude]
+        public string Name { get; set; }
+        [JsonInclude]
+        public int StartYear { get; set; }
+
+        [JsonInclude]
+        public bool Active { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set; }
+
+        public Formation() { }
+
+        public override string ToString() {
+            return $"Name:[{this.Name}]";
+        }
+
+    }
+}
---
+++ AocPingKeepAlive/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace AocPingKeepAlive {
+
+    class Program {
+
+        async static Task Main(string[] args) {
+
+            var parms = ParseArgs(args);
+            var url = parms["--url"].ToString();
+            var delay = Convert.ToInt32(parms["--delay"]);
+            await PingAlive.CallApi(url, delay);
+        }
+
+        static Dictionary<string, object> ParseArgs(string[] args) {
+            var parms = new Dictionary<string, object>();
+            string lastArg = null;
+            foreach(var arg in args) {
+
+                if(arg.Length > 1 && arg.Substring(0,2) == "--") {
+                    parms.Add(arg, null);
+                    lastArg = arg; // strip double dash
+                    continue;
+                }
+
+                parms[lastArg] = arg;
+            }
+            return parms;
+        }
+    }
+}
